8c0a309f24cb942a4513cf3a11be70ccc13930f4
lutzid <lut.zidan@gmail.com>
2020-05-18 13:15:32 +0700

    add a controller

---
+++ src/apps/modules/dashboard/Presentation/Web/Controller/OwnerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Its\Example\Dashboard\Presentation\Web\Controller;
+
+use Phalcon\Mvc\Controller;
+
+class OwnerController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function updateAction()
+    {
+
+    }
+
+    public function kostAction()
+    {
+        
+    }
+}
