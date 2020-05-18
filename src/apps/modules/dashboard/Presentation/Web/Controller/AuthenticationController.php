d02521efceb5a5f818fc96acdcbf0fa12cc97739
lutzid <lut.zidan@gmail.com>
2020-05-18 13:28:17 +0700

    add auth controller

---
+++ src/apps/modules/dashboard/Presentation/Web/Controller/AuthenticationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Its\Example\Dashboard\Presentation\Web\Controller;
+
+use Phalcon\Mvc\Controller;
+
+class AuthenticationController extends Controller
+{
+    public function registerAction()
+    {
+
+    }
+
+    public function createAction()
+    {
+        
+    }
+
+    public function loginAction()
+    {
+
+    }
+
+    public function logoutAction()
+    {
+
+    }
+}
