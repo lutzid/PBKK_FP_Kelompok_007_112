dfc755ecb2893b3528dfdf03095a363ea1c69648
lutzid <lut.zidan@gmail.com>
2020-05-19 01:34:07 +0700

    Update OwnerController.php

--- src/apps/modules/dashboard/Presentation/Web/Controller/OwnerController.php
+++ src/apps/modules/dashboard/Presentation/Web/Controller/OwnerController.php
@@ -6,18 +6,28 @@ use Phalcon\Mvc\Controller;
 
 class OwnerController extends Controller
 {
-    public function indexAction()
-    {
+    // public function indexAction()
+    // {
 
-    }
+    // }
 
-    public function updateAction()
-    {
+    // public function updateAction()
+    // {
 
-    }
+    // }
 
     public function kostAction()
     {
+        $id = $this->session->get('auth')->id;
+        $kost = Kost::findFirst(
+            [
+                "conditions" => "id_user = :id:",
+                "bind" => [
+                    "id" => $id
+                ]
+            ]
+        );
         
+        $this->view->kost = $kost;
     }
 }
