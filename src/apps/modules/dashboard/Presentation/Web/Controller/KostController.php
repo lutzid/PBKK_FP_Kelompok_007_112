55245113503f9c50210abc23e34074e2d6866aa4
Pristi Zahara <pristizzz@gmail.com>
2020-05-18 13:16:47 +0700

    init views

---
+++ src/apps/modules/dashboard/Presentation/Web/Controller/KostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Its\Example\Dashboard\Presentation\Web\Controller;
+
+use Phalcon\Mvc\Controller;
+
+class KostController extends Controller
+{
+    public function indexAction()
+    { }
+
+    public function createAction()
+    { }
+
+    public function storeAction()
+    { }
+
+    public function showAction()
+    { }
+
+    public function editAction()
+    { }
+
+    public function updateAction()
+    { }
+
+    public function destroyAction()
+    { }
+}
